d179c1bef5627796922b35fdefbe219f6f0b6720
Chris Higgins <chris@chigs.me>
2014-06-23 14:18:13 -0500

    Reworded alert message

--- cdw/static/admin_common.js
+++ cdw/static/admin_common.js
@@ -2,6 +2,6 @@
 if(!window.tools){window.tools={};}
 tools.notImplemented=function(){alert('Not yet implemented');}
 tools.bodyClass=tools.bodyClasses=function(klasses,fn){if(klasses.indexOf(' ')>=0){klasses=klasses.split(/\s+/);}
-$(function(){var $body=$('body');if(klasses.join){if($body.is('.'+klasses.join(',.'))){fn();}}else{if($body.hasClass(klasses)){fn();}}});};$(function(){$('form.delete-form button[type=submit]').click(function(e){if(confirm("Are you sure you want to delete this item? This cannot be undone.")){return true}
+$(function(){var $body=$('body');if(klasses.join){if($body.is('.'+klasses.join(',.'))){fn();}}else{if($body.hasClass(klasses)){fn();}}});};$(function(){$('form.delete-form button[type=submit]').click(function(e){if(confirm("All data (including responses) associate with this question will be permanently deleted.  Do you want to continue?")){return true}
 e.preventDefault();});$('form.archive-form button[type=submit]').click(function(e){if(confirm("Are you sure you want to archive this item? This cannot be undone.")){return true}
 e.preventDefault();});});
--- cdw/static/js/app/admin.js
+++ cdw/static/js/app/admin.js
@@ -5,7 +5,7 @@
 
 $(function(){
   $('form.delete-form button[type=submit]').click(function(e){
-    if(confirm("Are you sure you want to delete this item? This cannot be undone.")) {
+    if(confirm("All data (including responses) associate with this question will be permanently deleted.  Do you want to continue?")) {
       return true
     }
     e.preventDefault();
